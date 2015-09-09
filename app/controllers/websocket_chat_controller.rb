c171de7c692a955241f5f9cf41dd27bcc7c620e3
tomihei <lolvgmxd@gmail.com>
2015-09-10 00:10:29 +0900

    adfa

--- app/controllers/websocket_chat_controller.rb
+++ app/controllers/websocket_chat_controller.rb
@@ -18,18 +18,23 @@ class WebsocketChatController < WebsocketRails::BaseController
     logger.debug("connected user")
     puts "connect_user"
     talks = controller_store[:redis].lrange gid, 0,-1
+    puts "co"
     talks.each do |message|
       msg = ActiveSupport::HashWithIndifferentAccess.new(eval(message))
       send_message :websocket_chat,msg 
     end
+    puts "d"
   end
 
   def new_message  
     #クライアントからのメッセージを取得
-    logger.debug("Call new_message ") 
+    logger.debug("Call new_message ")
+    puts "new"
     gid = message[:group_id]
     message[:time] = Time.now.strftime("%Y/%m/%d %H:%M:%S").to_s
+    puts "a"
     message[:client_id] = client_id
+    puts "b"
     #レス番号付加
     talknum = controller_store[:redis].llen gid
     message[:resnum] = talknum + 1
