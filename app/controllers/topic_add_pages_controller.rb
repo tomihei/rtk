7dd6aaeaef7edc7a2110ea4fe0cae2b50279f536
tomihei <lolvgmxd@gmail.com>
2015-10-28 15:48:21 +0900

    一覧表示変更

--- app/controllers/topic_add_pages_controller.rb
+++ app/controllers/topic_add_pages_controller.rb
@@ -33,7 +33,7 @@ class TopicAddPagesController < ApplicationController
    allkey.each do |topic|
     #トピックデータ取り出し配列形式
     @list["#{topic[:key]}"] = $redistopic.hmget("#{topic[:key]}","title","rescount","visitor","lastpost","buildtime")
-    cont = $rediscont.lpop topic[:key] 
+    cont = $rediscont.lrange topic[:key],0,0
     @list["#{topic[:key]}"].insert(5,cont)
    end
   end
