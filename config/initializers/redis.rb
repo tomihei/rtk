e31c150840950a7d62becb0efdce07623f0a49b4
tomihei <lolvgmxd@gmail.com>
2018-05-06 17:44:52 +0900

    redis 設定変更

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -6,7 +6,7 @@ end
 
 if ENV["REDISCLOUD_URL"]
   uri = URI.parse(ENV["REDISCLOUD_URL"])
-  $redistopic = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :driver => "hiredis")
+  $rediscont = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :driver => "hiredis")
 else
   $rediscont  = Redis.new(:host => "localhost", :port => 6379 ,:db => 1,:driver => "hiredis") 
 end
